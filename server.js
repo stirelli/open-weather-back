ee6658f6a5798c65bf1d82d4268efb31c67060d5
Sebastian Tirelli <sebastian.tirelli@altoros.com>
2021-05-09 21:03:03 -0300

    add express server

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config({ silent: process.env.NODE_ENV === 'production' })
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const compression = require('compression')
+const cors = require('cors')
+const app = express()
+
+app.set('port', process.env.PORT || 3000)
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+app.use(
+  bodyParser.json({
+    limit: '20mb'
+  })
+)
+app.use(
+  bodyParser.urlencoded({
+    limit: '20mb',
+    extended: true
+  })
+)
+
+app.use(cors())
+app.use(compression())
+app.listen(app.get('port'))
+
+module.exports = app
